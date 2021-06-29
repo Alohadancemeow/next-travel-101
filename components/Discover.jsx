c59c2e55cbdd356a33a7c092a4cae3b7ec47f989
Alohadancemeow <nawongsak@gmail.com>
2021-06-29 15:34:56 +0700

    discover section

--- components/Discover.jsx
+++ components/Discover.jsx
@@ -1,11 +1,181 @@
 import React from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
+import styled from 'styled-components'
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+import SwiperCore, { EffectCoverflow, Autoplay } from 'swiper';
+import 'swiper/swiper.min.css';
+import "swiper/components/effect-coverflow/effect-coverflow.min.css"
 
 const Discover = () => {
+
+    SwiperCore.use([EffectCoverflow, Autoplay]);
+
+    const discoverData = [
+        {
+            id: 1,
+            title: 'Muk',
+            imageUrl: '/images/discover1.jpg',
+            desc: '24 hours available'
+        },
+        {
+            id: 2,
+            title: 'Muk',
+            imageUrl: '/images/discover2.jpg',
+            desc: '24 hours available'
+        },
+        {
+            id: 3,
+            title: 'Muk',
+            imageUrl: '/images/discover3.jpg',
+            desc: '24 hours available'
+        },
+        {
+            id: 4,
+            title: 'Muk',
+            imageUrl: '/images/discover4.jpg',
+            desc: '24 hours available'
+        },
+        {
+            id: 5,
+            title: 'Muk',
+            imageUrl: '/images/discover1.jpg',
+            desc: '24 hours available'
+        },
+    ]
+
     return (
-        <div>
-            <h1>Discover section</h1>
-        </div>
+        <DiscoverSection id="discover">
+            <DiscoverTitle>Discover <br /> section</DiscoverTitle>
+            <DiscoverContainer>
+                <Swiper
+                    effect='coverflow'
+                    grabCursor={true}
+                    centeredSlides={true}
+                    slidesPerView={3}
+                    loop={true}
+                    spaceBetween={32}
+                    coverflowEffect={{
+                        rotate: 0,
+                    }}
+                    autoplay={{
+                        delay: 2500,
+                        disableOnInteraction: false
+                    }}
+                >
+                    {
+                        discoverData.map(item => (
+                            <SwiperSlide key={item.id}>
+                                <DiscoverCard>
+                                    <ImageBox>
+                                        <img src={item.imageUrl} alt="discover-images" />
+                                    </ImageBox>
+                                    <TextBox>
+                                        <Title>{item.title}</Title>
+                                        <Description>{item.desc}</Description>
+                                    </TextBox>
+                                </DiscoverCard>
+                            </SwiperSlide>
+                        ))
+                    }
+
+                </Swiper>
+            </DiscoverContainer>
+        </DiscoverSection >
     )
 }
 
 export default Discover
+
+
+// # Styled-components
+const DiscoverSection = styled.section`
+    padding: 4.5rem 0 2.5rem;
+
+    @media screen and (min-width: 768px) {
+        padding: 7rem 0 2rem;
+    }
+`
+
+const DiscoverTitle = styled.h2`
+    font-size: 1.25rem;
+    color: hsl(190, 64%, 18%);
+    text-align: center;
+    text-transform: capitalize;
+    margin-bottom: 2rem;
+`
+
+const DiscoverContainer = styled.div`
+    max-width: 968px;
+    margin-left: 1rem;
+    margin-right: 1rem;
+    position: relative;
+
+    @media screen and (min-width: 768px) {
+        padding-top: 2rem;
+        width: 610px;
+        margin-left: auto;
+        margin-right: auto;
+    }
+
+    @media screen and (min-width: 1024px) {
+        width: 700px;
+    }
+
+    @media screen and (min-width: 1200px) {
+        width: 1024px;
+    }
+`
+
+const DiscoverWrapper = styled.div``
+
+const DiscoverCard = styled.div`
+    position: relative;
+    /* width: auto; */
+    /* width: 200px; */
+    overflow: hidden;
+
+
+    @media screen and (min-width: 1024px) {
+        /* width: 237px; */
+        width: auto;
+    }
+`
+
+const ImageBox = styled.div`
+    /* width: 100%; */
+
+    img {
+        /* max-width: fit-content; */
+        /* height: 350px; */
+        /* height: fit-content; */
+        transition: 0.3s;
+
+        &:hover {
+            transform: scale(1.1);
+        }
+    }
+`
+
+const TextBox = styled.div`
+    position: absolute;
+    bottom: 1.5rem;
+    left: 1rem;
+`
+
+const Title = styled.h2`
+    color: #fff;
+    font-size: 1rem;
+    margin-bottom: 0.25rem;
+
+    @media screen and (min-width: 1024px) {
+        font-size: 1.75rem;
+    }
+`
+
+const Description = styled.span`
+    color: #fff;
+    font-size: 0.75rem;
+    display: block;
+`
